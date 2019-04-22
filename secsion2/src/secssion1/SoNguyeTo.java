e4a90950bd89a81b6762e7ad601649971ee23e78
tandat01 <dat01101999@gmail.com>
2019-04-22 18:34:00 +0700

    ass2

---
+++ secsion2/src/secssion1/SoNguyeTo.java
@@ -0,0 +1,45 @@
+package secssion1;
+
+import java.util.Scanner;
+
+public class SoNguyeTo {
+    public static void main(String[] args) {
+        int n;
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Nhap so n:");
+        n=scanner.nextInt();
+        if (n>0){
+            if (kiemtrasonguyeto(n)==1)
+                System.out.println("La so nguyen to:"+n);
+            else
+                System.out.println("Khong phai la so nguyen to");
+
+        }
+    }
+    public static int kiemtrasonguyeto(int n){
+        int tem=0;
+        for (int i = 1;i<=n;i++){
+            if (n%i==0)
+                tem++;
+        }
+        if (tem==2)
+            return 1;
+
+        else return 0;
+    }
+
+
+
+    public void timSoNguyeTo(){
+
+
+    }
+    public void GanVao(){
+
+    }
+}
+
+
+
+
+
